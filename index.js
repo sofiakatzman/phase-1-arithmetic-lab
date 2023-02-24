92bfa8a05a3d562ecf8c7dc2a73fb7fb42632d2f
Sofia Katzman <katzmansof@gmail.com>
2023-02-24 15:29:59 -0500

    Finished Lab!

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// Write your code
+let num1 = 1;
+let num2 = 62; 
+let num3 = 4;
+let num4 = 1;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Number.parseInt((Math.random() * 10) + 1);
+let mod = num3 / num4;
+let max = Math.max(1, 3, 5, 12, 20);
